29f134afff4a07559c668b9cbe53f5d28b4891d1
hieptanthu <nguyenhiep24102002@gmail.com>
2024-12-15 08:32:37 +0700

    ok

--- fe/src/pages/_layout.js
+++ fe/src/pages/_layout.js
@@ -1,26 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useContract } from "@/context/NFTMarketplaceContext";
 import { Button } from "react-bootstrap"; // Make sure you import from 'react-bootstrap'
-import UserApi from "@/api/userAPi";
+
 
 function Layout({ children }) {
   const { account, accountHandler } = useContext(useContract);
-
-  // ["none", "notAcc", "haveAcc"]
-  const [checkAcc, setCheckAcc] = useState("");
-
-  const [showCreateAcc, setShowCreateAcc] = useState(false);
-
-  useEffect(() => {
-    const callUser = async () => {
-      const data = await UserApi.getById(account);
-      if (data) {
-        setCheckAcc("notAcc");
-      } else {
-        setShowCreateAcc();
-      }
-    };
-  }, [account]);
 
   if (!account) {
     return (
